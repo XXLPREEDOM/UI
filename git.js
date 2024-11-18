152e14fa936de5c80034c1026a754ad2cd4f15d5
XXLPREEDOM <1215177174@qq.com>
2024-11-19 00:02:11 +0800

    feat: update

--- git.js
+++ git.js
@@ -1,4 +1,7 @@
 const { simpleGit } = require('simple-git')
 const git = simpleGit({ baseDir: './' })
 console.log('git', git.status());
-git.add('.')
+const res = git.add('.')
+console.log('res', res.add);
+// git.commit("update")
+// git.push()
